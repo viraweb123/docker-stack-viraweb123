ab2ef0909d67f205fca5bea397352d939ae5bebc
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-10-22 12:38:30 +0330

    change marketplace address

--- pluf/html/config.php
+++ pluf/html/config.php
@@ -117,7 +117,7 @@ return array(
         )
     ),
 
-    'marketplace.backend' => 'http://marketplace.digidoki.com',
+    'marketplace.backend' => 'http://marketplace.viraweb123.ir',
 
     // -------------------------------------------------------------
     // SEO
