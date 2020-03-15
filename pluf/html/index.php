4b0aa3b7cb8094bd6b119f31456caf0589a09b5f
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2020-03-15 18:29:53 +0330

    edit configs

--- pluf/html/index.php
+++ pluf/html/index.php
@@ -1,6 +1,10 @@
 <?php
+
+use Pluf\Module;
+
 require __DIR__ . '/../vendor/autoload.php';
 require 'Pluf.php';
 Pluf::start('config.php');
-Pluf_Dispatcher::dispatch(Pluf_HTTP_URL::getAction(), 'urls.php');
+//Pluf_Dispatcher::dispatch(Pluf_HTTP_URL::getAction(), 'urls.php');
+Pluf_Dispatcher::dispatch(Pluf_HTTP_URL::getAction(), Module::loadControllers());
 
