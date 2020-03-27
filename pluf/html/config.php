50f47479b57da16dd9a6c9f0f8bf9be114de2f8e
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2020-03-27 12:54:12 +0430

    little change

--- pluf/html/config.php
+++ pluf/html/config.php
@@ -196,7 +196,7 @@ return array(
         'market',
         'marketplace'
     ),
-    'tenant_notfound_url' => 'https://viraweb123.ir/wb/shop/page/how-config-notfound-tenant',
+    'tenant_notfound_url' => 'https://viraweb123.ir/wb/shop/how-config-notfound-tenant',
 
     // -------------------------------------------------------------
     // super-tenant
