85c8a08279a759c8eb66ed492fa4ef122fb68c52
hadi2 <hadi2@linux-y1wf>
2020-10-20 18:34:23 +0330

    command to run lottery micro-system (docker)

--- microsys-lottery-start.sh
+++ microsys-lottery-start.sh
@@ -4,7 +4,6 @@ docker service create --name lottery \
     --replicas 1 \
     --replicas-max-per-node 1 \
     --network back-tier \
-    --mount type=bind,source=/mnt/apache,target=/etc/apache2 \
     --restart-condition any \
     docker.viraweb123.ir/lottery:6.0.17
     
